fdcd7807f122ee69643326f6b2fc3e8b356e993f
vuth860920 <120612003+vyle151122@users.noreply.github.com>
2023-07-25 21:54:21 +0700

    Create timing_3.py

---
+++ timing_3.py
@@ -0,0 +1,9 @@
+# import the time module
+import time
+
+# get the current time in seconds since the epoch
+seconds = time.time()
+
+print("Seconds since epoch =", seconds)	
+
+# Output: Seconds since epoch = 1672214933.6804628
